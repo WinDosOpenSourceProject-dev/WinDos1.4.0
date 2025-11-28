f09206d4dcacc6b7554d049684a376d6cd81c54f
WinDosOpenSourceProject-dev <alivarialivaria@gmail.com>
2025-11-28 20:20:54 +0200

    Create stdlib.c

---
+++ lib/stdlib.c
@@ -0,0 +1,71 @@
+int atoi(const char* str) {
+    int result = 0;
+    int sign = 1;
+    
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    }
+    
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    
+    return sign * result;
+}
+
+void itoa(int num, char* str, int base) {
+    int i = 0;
+    int is_negative = 0;
+    
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
+    
+    if (num < 0 && base == 10) {
+        is_negative = 1;
+        num = -num;
+    }
+    
+    while (num != 0) {
+        int rem = num % base;
+        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / base;
+    }
+    
+    if (is_negative) {
+        str[i++] = '-';
+    }
+    
+    str[i] = '\0';
+    
+    // Reverse the string
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void* malloc(size_t size) {
+    static char heap[64 * 1024];
+    static size_t heap_ptr = 0;
+    
+    if (heap_ptr + size > sizeof(heap)) {
+        return NULL;
+    }
+    
+    void* ptr = &heap[heap_ptr];
+    heap_ptr += size;
+    return ptr;
+}
+
+void free(void* ptr) {
+}
